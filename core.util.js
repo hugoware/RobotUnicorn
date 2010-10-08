1b2e9dbd260cc5ff977cbfbc346d263781bba895
hugoware <hugo@hugoware.net>
2010-10-07 19:12:19 -0500

    modfied core.util with new functionality

--- core.util.js
+++ core.util.js
@@ -10,7 +10,21 @@ ru.core.util = {
     
     //formats a string value
     format:function() {
-        return arguments[0];
+        
+        //verify the parameters are correct
+        if (arguments.length == 0) return "";
+        var params = ru.core.util.toArray(arguments);
+        
+        //extract the correct arguments
+        var message = (params[0] || "").toString();
+        params = params.splice(1, params.length - 1);
+        
+        //return the formatted information
+        return message.replace(/{\d+}/g, function(match) {
+            var index = parseInt(match.substr(1, match.length - 2));
+            return (params[index] || "").toString();
+        });
+        
     }
 
 };
