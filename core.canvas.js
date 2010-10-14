ab983034468e540d9f8794d62f1984c53ef24a4f
hugoware <hugo@hugoware.net>
2010-10-14 13:55:39 -0500

    quick changes for resources and added draw method

--- core.canvas.js
+++ core.canvas.js
@@ -21,6 +21,30 @@ ru.canvas = (function() {
             return self.instance;
         },
 
+        //causes all sprites to be drawn
+        update:function() {
+
+            //render each of the sprites into the view
+            ru.util.each(self.sprites, function(sprite) {
+
+                //update the resource if needed
+                sprite.resource = typeof sprite.resource == "string"
+                    ? ru.resource.find.image(sprite.resource)
+                    : sprite.resource;
+
+                //draw the image
+                self.context.drawImage(
+                    sprite.resource,
+                    sprite.x,
+                    sprite.y,
+                    sprite.width,
+                    sprite.height
+                    );
+
+            });
+
+        },
+
         //creates the canvas in the html
         init:function(params) {
 
