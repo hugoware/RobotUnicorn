c1a8ef167b200a45628885c965b06e2040fa8d90
hugoware <hugo@hugoware.net>
2010-10-31 00:48:56 -0500

    adjustments to 'bot' files

--- keagan.bot.js
+++ keagan.bot.js
@@ -2,19 +2,20 @@ var keaganbot = function() {
     var self = {
     
         right:true,
-        speed:3,
+        speed:5,
     
     
         //game: current state of the game
         //unit: the position and actions for your bot
         update:function(game, unit) {
-			self.right = self.right == true && unit.position.x < 499 ? true : false;
-			self.right = self.right == false && unit.position.x > 0 ? false : true;
-            unit.position.x += self.right ? self.speed : -self.speed;
+            unit.position.x += self.speed;
         }
     };
     
-    //the update options
+    //must expose an update function
     this.update = self.update;
-    this.image = "resources/player-2-ship.png";
+    
+    //name and preferred weapons
+    this.name = "KeaganBot";
+    this.weapons = ["burst", "lazer", "tracker"];
 };
